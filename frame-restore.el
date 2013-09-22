8298be2628f5ecb56455484fc1bddaa59e38e335
Sebastian Wiesner <lunaryorn@gmail.com>
2013-09-22 16:34:25 +0200

    Release 0.4

--- frame-restore.el
+++ frame-restore.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
 ;; URL: https://github.com/lunaryorn/frame-restore.el
 ;; Keywords:  frames convenience
-;; Version: 0.4-cvs
+;; Version: 0.4
 ;; Package-Requires: ((dash "1.2") (f "0.11") (emacs "24.1"))
 
 ;; This file is not part of GNU Emacs.
